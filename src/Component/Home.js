f8c50b07107449eff649137ef57f2b2dec9ccd48
Milan Tyagi <milantyagi2004@gmail.com>
2024-07-20 15:12:57 +0530

    fixing length

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -11,15 +11,13 @@ import Project1 from './Project1'
 function Home() {
   const videos = [
     { title: 'video 1', src:"video1.mp4" },
-    { title: 'Video 2', src: "video.mp4" },
+    { title: 'Video 2', src: "video.mp4" }
     // { title: 'Video 3', src: Svid3 },
     // { title: 'Video 3', src: Svid4 }
   ];
   const LVideo = [
     { title: 'Short reel', src:"video1.mp4" },
     { title: 'Video 2', src: 'video2.mp4' },
-    { title: 'Video 3', src: 'video3.mp4' },
-    { title: 'Video 4', src: 'video4.mp4' },
   ];
 
   return (
