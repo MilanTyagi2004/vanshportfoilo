f46132ab03c60008b578b33e8d1c5f666f9bd8e2
Milan Tyagi <milantyagi2004@gmail.com>
2024-08-15 16:51:23 +0530

    adding short video

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -5,19 +5,13 @@ import Profile from "../Component/Profile"
 import Footer from '../Component/Footer'
 import Contact from './Contact'
 import Project from '../Component/Project'
-import Project1 from './Project1'
-
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/vide2.mp4'
 
 function Home() {
   const videos = [
-    { title: 'video 1', src:"video1.mp4" },
-    { title: 'Video 2', src: "video.mp4" }
-    // { title: 'Video 3', src: Svid3 },
-    // { title: 'Video 3', src: Svid4 }
-  ];
-  const LVideo = [
-    { title: 'Short reel', src:"video1.mp4" },
-    { title: 'Video 2', src: 'video2.mp4' },
+    { title: 'video 1', src: video1},
+    { title: 'Video 2', src: video2}
   ];
 
   return (
@@ -25,7 +19,6 @@ function Home() {
       <Header/>
       <Profile/>
       <About/>
-      <Project1 videos = {LVideo}/>
       <Project videos={videos}/>
       <Contact/>
       <Footer/>
